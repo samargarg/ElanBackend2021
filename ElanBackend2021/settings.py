1214ff10b3d301d3ee966ced16d6bd95293194dc
samargarg <samargarg54@gmail.com>
2021-02-06 10:20:13 +0530

    Integrated Contact Us

--- ElanBackend2021/settings.py
+++ ElanBackend2021/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'caportal',
+    'mainsite',
     'rest_framework',
     'rest_framework.authtoken'
 ]
--- ElanBackend2021/urls.py
+++ ElanBackend2021/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('caportal/', include('caportal.urls'))
+    path('caportal/', include('caportal.urls')),
+    path('mainsite/', include('mainsite.urls'))
 ]
